dc35e8ae2a705dd58c3f36f1931af816b44a8f71
Jack Schwarz <jackschwarz58@gmail.com>
2020-04-23 01:08:58 -0500

    Began implementation of Analytics page and bug fixes.
    Began working on fine tuning the CSS of the Analytics page and setting up Javascript functions to create the data. This included fixing some syntax inconsistencies across other pages and merging an obsolete CSS file with another labeled 'TEST'. Also fixed a long-standing bug that resulted in some pages loading Frappe.io elements outside their div container.

--- resources/scripts/analytics.js
+++ resources/scripts/analytics.js
@@ -1,90 +1,121 @@
-var startDate = new Date(2019, 12, 1);
-var endDate = new Date(2020, 12, 31);
-var hmap = new frappe.Chart("#heat-map", {
-    data:{
-        dataPoints: {
-            /*"1426744959": 20,
-            "1463673055": 113,
-            "1476892421": 57,*/
-            // ...
-        },
-        start: startDate, // a JS date object
-        end: endDate,
-    },
-    type: 'heatmap', //'bar', 'line', 'scatter', 'pie', 'percentage'
-    colors: ['#e1daf2', '#c9b9f0', '#9c7bed', '#6a34ed', '#250478'],
+var salesData;
+var todayDate = new Date();
+
+$.ajax({
+    url: "/file/pullSalesData",
+    method: 'GET',
+    async:false,
+    success: function(data){
+        salesData = data;
+    }
 });
 
-// Sales to Ratio Graph
-let chart = new frappe.Chart( "#chart", { // or DOM element
-    data: {
-      labels: ["YS", "BS", "GS", "RS",
-      "BlJ", "NJ", "DJ", "FJ", "GrH", "CH", "MH"],
+window.onload = function() {
+    createMonthlySales();
+    createProfitLoss();
+    createSalesRatio();
+    createForecastDemand();
+};
 
-      datasets: [
-        {
-          name: "Inventory", chartType: 'bar',
-          values: [3220, 2959, 2015, 919, 
-            350, 661, 850, 902, 571, 399, 408]
+
+function createMonthlySales(){
+    var startDate = new Date(todayDate.getFullYear() - 1, 12, 1);
+    var endDate = new Date(todayDate.getFullYear(), 12, 31);
+    var hmap = new frappe.Chart("#heat-map", {
+        data:{
+            dataPoints: {
+                "1587620791": 2000, //These are Time-Stamps followed by data values
+                "1463673055": 113,
+                "1476892421": 57,
+            },
+            start: startDate, // a JS date object
+            end: endDate,
         },
-        {
-          name: "Sales", chartType: 'line',
-          values: [1390, 1239, 839, 234,
-            298, 378, 502, 436, 401, 249, 273]
-        }
-      ]
-    },
+        type: 'heatmap',
+        colors: ['#e1daf2', '#c9b9f0', '#9c7bed', '#6a34ed', '#250478'],
+    });
+}
 
-    title: "Sales to Inventory Ratio",
-    type: 'axis-mixed', // or 'bar', 'line', 'pie', 'percentage'
-    colors: ['purple', '#ffa3ef', 'light-blue'],
-  });
+function createSalesRatio() {
+    // Sales to Ratio Graph
+    let chart = new frappe.Chart( "#chart", { // or DOM element
+        data: {
+            labels: ["YS", "BS", "GS", "RS",
+                     "BlJ", "NJ", "DJ", "FJ", "GrH", "CH", "MH"],
 
-  // dummy chart for right module
-  let right = new frappe.Chart( "#right", { // or DOM element
-    data: {
-      labels: ["YS", "BS", "GS", "RS",
-      "BlJ", "NJ", "DJ", "FJ", "GrH", "CH", "MH"],
-
-      datasets: [
-        {
-          name: "Inventory", chartType: 'bar',
-          values: [3220, 2959, 2015, 919, 
-            350, 661, 850, 902, 571, 399, 408]
+            datasets: [
+                {
+                    name: "Inventory", chartType: 'bar',
+                    values: [3220, 2959, 2015, 919, 
+                             350, 661, 850, 902, 571, 399, 408]
+                },
+                {
+                    name: "Sales", chartType: 'line',
+                    values: [1390, 1239, 839, 234,
+                             298, 378, 502, 436, 401, 249, 273]
+                }
+            ]
         },
-        {
-          name: "Sales", chartType: 'line',
-          values: [1390, 1239, 839, 234,
-            298, 378, 502, 436, 401, 249, 273]
-        }
-      ]
-    },
 
-    title: "Sales to Inventory Ratio",
-    type: 'axis-mixed', // or 'bar', 'line', 'pie', 'percentage'
-    colors: ['purple', '#ffa3ef', 'light-blue'],
-  });
-    
-  let regression = new frappe.Chart( "#regression", { // or DOM element
-    data: {
-      labels: ["YS", "BS", "GS", "RS",
-      "BlJ", "NJ", "DJ", "FJ", "GrH", "CH", "MH"],
-
-      datasets: [
-        {
-          name: "Inventory", chartType: 'scatter',
-          values: [3220, 2959, 2015, 919, 
-            350, 661, 850, 902, 571, 399, 408]
+        title: "Sales to Inventory Ratio",
+        type: 'axis-mixed', // or 'bar', 'line', 'pie', 'percentage'
+        colors: ['purple', '#ffa3ef', 'light-blue'],
+        tooltipOptions: {
+            formatTooltipX: d => (d + '').toUpperCase(),
+            formatTooltipY: d => d + ' sold',
         },
-        {
-          name: "Sales", chartType: 'line',
-          values: [1390, 1239, 839, 234,
-            298, 378, 502, 436, 401, 249, 273]
-        }
-      ]
-    },
+    });
+}
 
-    title: "Sales to Inventory Ratio",
-    type: 'axis-mixed', // or 'bar', 'line', 'pie', 'percentage'
-    colors: ['purple', '#ffa3ef', 'light-blue'],
-  });
+function createProfitLoss() {
+    // dummy chart for right module
+    let right = new frappe.Chart( "#right", { // or DOM element
+        data: {
+            labels: ["YS", "BS", "GS", "RS",
+                     "BlJ", "NJ", "DJ", "FJ", "GrH", "CH", "MH"],
+
+            datasets: [
+                {
+                    name: "Inventory", chartType: 'bar',
+                    values: [3220, 2959, 2015, 919, 
+                             350, 661, 850, 902, 571, 399, 408]
+                },
+                {
+                    name: "Sales", chartType: 'line',
+                    values: [1390, 1239, 839, 234,
+                             298, 378, 502, 436, 401, 249, 273]
+                }
+            ]
+        },
+
+        title: "Sales to Inventory Ratio",
+        type: 'axis-mixed', // or 'bar', 'line', 'pie', 'percentage'
+        colors: ['purple', '#ffa3ef', 'light-blue'],
+    });
+}
+
+function createForecastDemand() {
+    let regression = new frappe.Chart( "#regression", { // or DOM element
+        data: {
+            labels: ["YS", "BS", "GS", "RS",
+                     "BlJ", "NJ", "DJ", "FJ", "GrH", "CH", "MH"],
+
+            datasets: [
+                {
+                    name: "Inventory", chartType: 'scatter',
+                    values: [3220, 2959, 2015, 919, 
+                             350, 661, 850, 902, 571, 399, 408]
+                },
+                {
+                    name: "Sales", chartType: 'line',
+                    values: [1390, 1239, 839, 234,
+                             298, 378, 502, 436, 401, 249, 273]
+                }
+            ]
+        },
+
+        title: "Sales to Inventory Ratio",
+        type: 'axis-mixed', // or 'bar', 'line', 'pie', 'percentage'
+        colors: ['purple', '#ffa3ef', 'light-blue'],
+    });
+}
